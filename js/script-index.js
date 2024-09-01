8d7aa0bf01e6c0366fd7e97de8c90841396d6664
Lux_ <80638883+Luxinenglish@users.noreply.github.com>
2024-09-02 01:18:39 +0200

    Delete js directory

--- js/script-index.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    var sidenav = document.querySelector('nav.sidenav');
-    var overlay = document.querySelector('.overlay');
-    sidenav.classList.toggle('active');
-    overlay.classList.toggle('active');
-}
